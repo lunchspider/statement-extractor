a718c5eab503f17cd782acc94ea8f0dc9ae0fdcf
lunchspider <lunchspider03@gmail.com>
2023-11-10 15:19:51 +0530

    added code

--- src/main.py
+++ src/main.py
@@ -1,96 +1,86 @@
-# This script is designed to convert bank statements from pdf to excel.
-#
-# It has been tweaked on HDFC Bank Credit Card statements,
-# but in theory you can use it on any PDF document.
-#
-# The script depends on camelot-py,
-# which can be installed using pip #
-# pip install "camelot-py[cv]"
-
-
 import os
 import argparse
-import camelot
 import pandas as pd
-import numpy as np
-from collections import defaultdict
+import shutil
+from pypdf import PdfReader
 
-
-def handle_transaction(df: pd.DataFrame, start_index: int, end_index: int): 
-    start_index += 2;
-    info: str = df.loc[start_index][1]
-    start_index += 1
-    for i in range(start_index, end_index):
-        row = df.loc[i];
-        if(row[0].strip() == ''):
-            info = row[1]
-            continue
-        df.at[i, 'deleted'] = False
-        df.at[i,3] = info
-
-
-def extract_df(path):
-    # The default values from pdfminer are M = 2.0, W = 0.1 and L = 0.5
-    laparams = {'char_margin': 2.0, 'word_margin': 0.2, 'line_margin': 1.0}
-
-    # Extract all tables using the lattice algorithm
-    lattice_tables = camelot.read_pdf(path,  
-        pages='all', flavor='lattice', line_scale=50, layout_kwargs=laparams)
-
-    # Extract bounding boxes
-    regions = defaultdict(list)
-    for table in lattice_tables:
-        bbox = [table._bbox[i] for i in [0, 3, 2, 1]]
-        regions[table.page].append(bbox)
-
-    df = pd.DataFrame()
-
-    # Extract tables using the stream algorithm
-    for page, boxes in regions.items():
-        areas = [','.join([str(int(x)) for x in box]) for box in boxes]
-        stream_tables = camelot.read_pdf(path, pages=str(page),
-            flavor='stream', table_areas=areas, row_tol=5, layout_kwargs=laparams)
-        if(page == 5):
-            for table in stream_tables:
-                print(table.df)
-                print('we are here')
-        dataframes = [table.df for table in stream_tables]
-        dataframes = pd.concat(dataframes)
-        df = df._append(dataframes, ignore_index = True)
-
-    df['deleted'] = True;
-    transaction_rows, _ = np.where(df == 'Domestic Transactions');
-    last_row, _ = np.where(df == 'Reward Points Summary');
-    transaction_rows = np.concatenate([transaction_rows, last_row])
-    for j in range(0, len(transaction_rows) - 1):
-        handle_transaction(df, transaction_rows[j] ,transaction_rows[j + 1])
-    df = df[df['deleted'] != True]
-    del df['deleted']
-    new_record = pd.DataFrame([['Date', 'to', 'amount', 'card']])
-    df = pd.concat([new_record, df], ignore_index=True)
-
-    #df = df.drop([i for i in range(0, 13)])
-    return df
+def handle_file(file_name : str) -> dict[str, str]:
+    reader = PdfReader(file_name)
+    page = reader.pages[0]
+    text = page.extract_text()
+    arr = text.split('\n')
+    info = {}
+    print(arr)
+    for (index, i) in enumerate(arr):
+        if 'Sold By' in i:
+            x = i.replace('Sold By: ', '')
+            x = x.replace(',', '')
+            x = x.strip()
+            info['Seller Name'] = x
+        if 'Bill To' in i:
+            buyer = arr[index + 1].strip()
+            info['Buyer Name'] = buyer
+        if 'Invoice Date:' in i:
+            invoice_date = i.replace('Invoice Date:', '').strip()
+            info['invoice_date'] = invoice_date
+        if 'Order Date:' in i:
+            order_date = i.replace('Order Date:', '').strip()
+            info['order_date'] = order_date
+        if 'GSTIN' in i:
+            gstin = i.replace('GSTIN  -', '').strip()
+            info['Seller GSTIN'] = gstin
+        if 'Invoice Number' in i:
+            invoice_number = i.replace('Invoice Number #', '').replace('Tax Invoice', '').strip()
+            info['invoice_number']  = invoice_number
+        if 'IMEI' in i:
+            imei = i.split(' ')
+            imei = ''.join(imei[len(imei) - 2: len(imei)])
+            info['imei'] = imei
+        if 'Grand Total' in i:
+            i = arr[index - 1]
+            s = i.split(' ')
+            info['qty'] = s[1]
+            info['total'] = s[-1]
+            info['IGST'] = s[-2]
+            info['taxable_value'] = s[-3]
+        if 'Order ID:' in i:
+            info['Buyer GSTIN'] = i.split(' ')[0]
+            info['order_id'] = arr[index + 1].strip()
+        if 'HSN' in i:
+            s = i.replace('HSN/SAC: ', '').strip()
+            hsn = ''
+            for j in s:
+                if not j.isdigit():
+                    break
+                hsn += j
+            info['HSN'] = hsn
+            s = s.replace(hsn, '') + arr[index + 1].strip()
+            info['product_name'] = s
+    return info
 
 
 def main(args):
+    result = []
+    if not os.path.isdir(args.out_dir):
+        os.makedirs(args.out_dir)
     for file_name in os.listdir(args.in_dir):
         root, ext = os.path.splitext(file_name)
         if ext.lower() != '.pdf':
             continue
         pdf_path = os.path.join(args.in_dir, file_name)
         print(f'Processing: {pdf_path}')
-        df = extract_df(pdf_path)
-        excel_name = root + '.xlsx'
-        excel_path = os.path.join(args.out_dir, excel_name)
-        df.to_excel(excel_path, index = False, header = False)
-        print(f'Processed : {excel_path}')
-        
+        info = handle_file(pdf_path)
+        out_path = os.path.join(args.out_dir, f'{info["order_id"]}-{info["invoice_number"]}.pdf')
+        shutil.copyfile(pdf_path, out_path)
+        result.append(info)
+    df = pd.DataFrame(result)
+    df.to_excel(args.out_file, index = False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--in-dir', type=str, required=True, help='directory to read statement PDFs from.')
-    parser.add_argument('--out-dir', type=str, required=True, help='directory to store statement XLSX to.')
+    parser.add_argument('--out-file', type=str, required=True, help='file to store the output excel.')
+    parser.add_argument('--out-dir', type=str, required=True, help='directory to write PDFs to.')
     #parser.add_argument('--password', type=str, default=None, help='password for the statement PDF.')
     args = parser.parse_args()
 
